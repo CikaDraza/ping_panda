696ed3961107e4480b218fd3e4fe2b9cee8f3b08
CikaDraza <drazic.milan@gmail.com>
2025-01-09 20:35:01 +0100

    bento grid, users reviews

--- src/app/components/heading.tsx
+++ src/app/components/heading.tsx
@@ -1,15 +1,35 @@
-import { cn } from '@/utils'
-import React, { HTMLAttributes, ReactNode } from 'react'
+import React, { ReactNode } from 'react';
+import { cn } from '@/utils';
 
-interface HeadingProps extends HTMLAttributes<HTMLHeadElement> {
-  children?: ReactNode
-  clasName: string
+interface HeadingProps {
+  children: ReactNode;
+  variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  className?: string;
 }
 
-export default function Heading({ children, className, ...props }:HeadingProps) {
+export default function Heading({ 
+  children, 
+  variant = 'h1', 
+  className = '', 
+  ...props 
+}: HeadingProps) {
+  const Tag = variant;
+  
   return (
-    <h1 className={cn("text-4xl sm:text-5xl text-pretty font-heading font-semibold tracking-tight text-zink-500")}>
-      { children }
-    </h1>
-  )
+    <Tag 
+      className={cn(
+        "font-heading font-semibold tracking-tight text-zinc-700", 
+        variant === 'h1' && "text-4xl sm:text-5xl",
+        variant === 'h2' && "text-3xl sm:text-4xl",
+        variant === 'h3' && "text-2xl sm:text-3xl",
+        variant === 'h4' && "text-xl sm:text-2xl",
+        variant === 'h5' && "text-lg sm:text-xl",
+        variant === 'h6' && "text-base sm:text-lg",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Tag>
+  );
 }
